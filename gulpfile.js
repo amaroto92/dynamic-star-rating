3811d0e06b1fee069a2021c796ca961d2788e8c8
Andrés Maroto <amaroto92@gmail.com>
2016-02-10 19:34:08 -0600

    [UPDATE] file name

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
+
+
+
 var gulp = require('gulp'),
 	minifyCss = require('gulp-minify-css'),
 	uglify = require('gulp-uglify'),
-	connect = require('gulp-connect');
+	connect = require('gulp-connect'),
+  rename = require('gulp-rename');
  
 gulp.task('webserver', function() {
   connect.server();
@@ -12,8 +16,10 @@ gulp.task('default', ['webserver']);
 gulp.task('compile',function(){
   gulp.src('dynamic-star-rating.css')
     .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(rename ('dynamic-star-rating.min.css'))
     .pipe(gulp.dest('dist'));
   gulp.src('dynamic-star-rating.js')
     .pipe(uglify())
+    .pipe(rename ('dynamic-star-rating.min.js'))
     .pipe(gulp.dest('dist'));
 });
